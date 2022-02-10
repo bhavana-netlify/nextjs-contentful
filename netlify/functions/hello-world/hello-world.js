ddba296f5a60c0b61bcf8a5d7f3727e4ae3d0cd1
bhavana <bhavana@netlify.com>
2022-02-10 14:48:04 -0500

    addding sceduled functuiins

---
+++ netlify/functions/hello-world/hello-world.js
@@ -0,0 +1,22 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const { schedule } = require('@netlify/functions')
+const handler = async (event) => {
+  console.log("Received event:", event)
+  try {
+    const subject = event.queryStringParameters.name || 'World'
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello ${subject}` }),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+module.exports = { handler }
+
+
+module.exports.handler = schedule("@hourly", handler);
