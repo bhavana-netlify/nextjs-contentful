bdc9578aa7bef4196cd20c6897158efec48e4ade
bhavana <bhavana@netlify.com>
2022-02-13 15:56:29 -0500

    adding a daily build

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -1,19 +1,16 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const { schedule } = require('@netlify/functions')
+const fetch = require('node-fetch')
+const REBUILD_URL = process.env.REBUILD
+
+
 const handler = async (event) => {
   console.log("Received event:", event)
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
+  await fetch(REBUILD_URL, {method: 'POST'});
+
+  return{
+    statusCode: 200,
+  };
 }
 
 module.exports = { handler }
